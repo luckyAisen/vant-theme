52072d9d7ce2125c885402156f9d2d628b4f708f
Aisen60 <caixusheng60@gmail.com>
2021-07-18 14:49:46 +0800

    build: update eslint rules and add ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,7 @@
 module.exports = {
   root: true,
-  parserOptions: {
-    ecmaVersion: 2020,
-    parser: '@typescript-eslint/parser'
-  },
   env: {
-    browser: true,
-    es6: true,
-    node: true
+    node: true,
   },
   extends: [
     "plugin:vue/vue3-essential",
@@ -16,17 +10,11 @@ module.exports = {
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
   ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
   rules: {
-    'prettier/prettier': 'error',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/member-delimiter-style': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/class-name-casing': 'off',
-    'vue/valid-v-slot': 'off',
-    'no-debugger': 'off',
-    'vue/experimental-script-setup-vars': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
