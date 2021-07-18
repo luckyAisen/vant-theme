776bc0e0ed59a42acd84ed0c239031c592216c6d
Aisen60 <caixusheng60@gmail.com>
2021-07-18 14:51:05 +0800

    build(deps): update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: [
-    [
-      'import',
-      {
-        libraryName: 'element-plus',
-        customStyleName: name => {
-          name = name.slice(3)
-          return `element-plus/packages/theme-chalk/src/${name}.scss`
-        }
-      }
-    ]
-  ]
+  presets: ['@vue/cli-plugin-babel/preset']
 }
