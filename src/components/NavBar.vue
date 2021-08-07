2120aa0653426bf86c062d8d528e8699424d3631
Aisen60 <caixusheng60@gmail.com>
2021-08-07 13:40:51 +0800

    feat: 添加导航信息

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,10 @@
       <router-link to="/"> vant-theme-preview </router-link>
     </div>
     <div class="navbar-action">
-      <a href="https://github.com/Aisen60/vant-theme.git" target="_blank">
+      <a href="https://vant-contrib.gitee.io/vant/v3/#/zh-CN/" target="_blank">
+        vant 官网
+      </a>
+      <a href="https://github.com/Aisen60/vant-theme" target="_blank">
         GitHub
       </a>
     </div>
