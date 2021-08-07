606228d3b92b531657725b4a3f57b14a2a68e8af
Aisen60 <caixusheng60@gmail.com>
2021-08-07 18:03:16 +0800

    fix: 路由模式修改为 hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import componentRoutes from './component'
 
 const routes: Array<RouteRecordRaw> = [
@@ -25,7 +25,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
