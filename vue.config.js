dcf15460ad2035db478eb02a628edadae9e85b6c
Aisen60 <caixusheng60@gmail.com>
2021-08-07 19:46:31 +0800

    chore: 修改 base url

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const resolve = dir => path.resolve(__dirname, dir)
 
 module.exports = {
+  publicPath:'./',
   chainWebpack: (config) => {
     config.resolve.alias
       .set('@', resolve('src'))
