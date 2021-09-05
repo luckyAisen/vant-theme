d020d033a935cc5081d2292da8dca2cb42aa0669
Aisen60 <caixusheng60@gmail.com>
2021-09-05 12:30:22 +0800

    feat: 修改网站标题名称

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
       filename: 'index.html',
       // 当使用 title 选项时，
       // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-      title: pkg.name
+      title: `${pkg.name} - ${pkg.description}`
       // 在这个页面中包含的块，默认情况下会包含
       // 提取出来的通用 chunk 和 vendor chunk。
     },
