1afe8fcdeb720ad609ffed1a6b65ab3f039d05ad
Aisen60 <Aisen60@users.noreply.github.com>
2021-12-03 21:04:24 +0000

    deploy: faccfd13e21aa8fde50eb22d470da3ba86b9c390

--- vant/v3/assets/iframe-router.745d6bc9.js
+++ vant/v3/assets/iframe-router.745d6bc9.js
@@ -1278,16 +1278,18 @@ components/
 
 <span class="hljs-keyword">function</span> <span class="hljs-title function_">usePageVisibility</span>(<span class="hljs-params"></span>): <span class="hljs-title class_">Ref</span>&lt;<span class="hljs-title class_">VisibilityState</span>&gt;;
 </code></pre></div><div class="van-doc-card"><h3 id="fan-hui-zhi" tabindex="-1">\u8FD4\u56DE\u503C</h3><table><thead><tr><th>\u53C2\u6570</th><th>\u8BF4\u660E</th><th>\u7C7B\u578B</th></tr></thead><tbody><tr><td>visibilityState</td><td>\u9875\u9762\u5F53\u524D\u7684\u53EF\u89C1\u72B6\u6001\uFF0C<code>visible</code> \u4E3A\u53EF\u89C1\uFF0C<code>hidden</code> \u4E3A\u9690\u85CF</td><td><em>Ref&lt;VisibilityState&gt;</em></td></tr></tbody></table></div>`,7),ca=[da],oa={setup(a,{expose:s}){return s({frontmatter:{}}),(n,t)=>(l(),p("div",pa,ca))}},ra={class:"van-doc-markdown-body"},ia=d(`<h1>useRect</h1><div class="van-doc-card"><h3 id="jie-shao" tabindex="-1">\u4ECB\u7ECD</h3><p>\u83B7\u53D6\u5143\u7D20\u7684\u5927\u5C0F\u53CA\u5176\u76F8\u5BF9\u4E8E\u89C6\u53E3\u7684\u4F4D\u7F6E\uFF0C\u7B49\u4EF7\u4E8E <a href="https://developer.mozilla.org/zh-CN/docs/Web/API/Element/getBoundingClientRect" target="_blank">Element.getBoundingClientRect</a>\u3002</p></div><h2 id="dai-ma-yan-shi" tabindex="-1">\u4EE3\u7801\u6F14\u793A</h2><div class="van-doc-card"><h3 id="ji-ben-yong-fa" tabindex="-1">\u57FA\u672C\u7528\u6CD5</h3><pre><code class="language-html"><span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">ref</span>=<span class="hljs-string">&quot;root&quot;</span> /&gt;</span>
-</code></pre><pre><code class="language-js"><span class="hljs-keyword">import</span> { ref } <span class="hljs-keyword">from</span> <span class="hljs-string">&#39;vue&#39;</span>;
+</code></pre><pre><code class="language-js"><span class="hljs-keyword">import</span> { ref, onMounted } <span class="hljs-keyword">from</span> <span class="hljs-string">&#39;vue&#39;</span>;
 <span class="hljs-keyword">import</span> { useRect } <span class="hljs-keyword">from</span> <span class="hljs-string">&#39;@vant/use&#39;</span>;
 
 <span class="hljs-keyword">export</span> <span class="hljs-keyword">default</span> {
   <span class="hljs-title function_">setup</span>(<span class="hljs-params"></span>) {
     <span class="hljs-keyword">const</span> root = <span class="hljs-title function_">ref</span>();
-    <span class="hljs-keyword">const</span> rect = <span class="hljs-title function_">useRect</span>(root);
 
-    <span class="hljs-variable language_">console</span>.<span class="hljs-title function_">log</span>(rect); <span class="hljs-comment">// -&gt; \u5143\u7D20\u7684\u5927\u5C0F\u53CA\u5176\u76F8\u5BF9\u4E8E\u89C6\u53E3\u7684\u4F4D\u7F6E</span>
-
+    <span class="hljs-title function_">onMounted</span>(<span class="hljs-function">()=&gt;</span>{
+      <span class="hljs-keyword">const</span> rect = <span class="hljs-title function_">useRect</span>(root);
+      <span class="hljs-variable language_">console</span>.<span class="hljs-title function_">log</span>(rect); <span class="hljs-comment">// -&gt; \u5143\u7D20\u7684\u5927\u5C0F\u53CA\u5176\u76F8\u5BF9\u4E8E\u89C6\u53E3\u7684\u4F4D\u7F6E</span>
+    })
+    
     <span class="hljs-keyword">return</span> { root };
   },
 };
