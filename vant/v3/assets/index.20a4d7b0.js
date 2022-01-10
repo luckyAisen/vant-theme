735755718c1a2c0543cf6f926fcd28fc92a2cb7e
Aisen60 <Aisen60@users.noreply.github.com>
2022-01-10 21:04:31 +0000

    deploy: 02691f0db2387f1a5497093e67f94131fdf9907c

--- vant/v3/assets/index.20a4d7b0.js
+++
@@ -1,1 +0,0 @@
-import"./use-translate.55dc92d8.js";import{w as s}from"./with-install.c8955fea.js";import{_ as t}from"./Picker.50685292.js";const e=s(t);export{e as P};
---
+++ vant/v3/assets/index.fee8ba2e.js
@@ -0,0 +1,1 @@
+import"./use-translate.55dc92d8.js";import{w as s}from"./with-install.c8955fea.js";import{_ as t}from"./Picker.449af095.js";const e=s(t);export{e as P};
