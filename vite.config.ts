a78f66f87fb23057bdc5a35c1e66f021b2139f02
Aisen60 <caixusheng60@gmail.com>
2022-03-02 17:34:07 +0800

    chore: base 配置添加前后斜杆

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "vant-theme",
+  base: "/vant-theme/",
 
   plugins: [vue(), vueJsx()],
 
