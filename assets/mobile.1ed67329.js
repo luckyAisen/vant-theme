4ceef61c51d03035766c6c148f06256f06d081bb
Aisen60 <Aisen60@users.noreply.github.com>
2022-03-02 09:03:31 +0000

    deploy: 148d6129282e3e407ce3e8ef51a46fcd54c3678a

---
+++ assets/mobile.1ed67329.js
@@ -0,0 +1,1 @@
+import{s as t,a as E,c as r}from"./index.22238fa8.js";import{V as S,a as _,b as d}from"./constant.150fe093.js";window.addEventListener("message",a=>{if(a.data.type===S){const e=JSON.parse(a.data.value);t(e)}if(a.data.type===_){const{varName:e,varValue:s}=JSON.parse(a.data.value);E(e,s)}a.data.type===d&&r()});
