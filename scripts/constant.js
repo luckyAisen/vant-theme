0b83f4a605d2a07449cf4b9a178fa566c74c2ad9
Aisen60 <caixusheng60@gmail.com>
2022-03-15 13:09:36 +0800

    feat: 修改 mobile 页面地址

--- scripts/constant.js
+++ scripts/constant.js
@@ -4,7 +4,7 @@ export const VERSION_LIST = ["v2", "v3", "v4"];
 export const VANT_GIT_REPO = "youzan/vant#gh-pages";
 export const VANT_SOURCE_LOCAL = ".VANT_SOURCE_LOCAL";
 export const VANT_PUBLIC_PATH = "public/vant";
-export const VANT_MOBILE_PAGE_CONCAT_PATH = (v) => `src/mobile/${v}.html`;
+export const VANT_MOBILE_PAGE_CONCAT_PATH = (v) => `src/page/mobile/${v}.html`;
 export const VANT_MENU_CONCAT_JSON = (v, language = "zh-cn") =>
   `src/json/menus/${language}/${v}.json`;
 export const VANT_STYLES_CONCAT_JSON = (v) => `src/json/styles/${v}.json`;
