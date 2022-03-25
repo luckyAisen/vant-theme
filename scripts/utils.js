fd0bbc845b6a58103e150663c7ddf9a8b8ad2f5a
Aisen60 <caixusheng60@gmail.com>
2022-03-26 01:54:06 +0800

    ci: modify scripts

--- scripts/utils.js
+++ scripts/utils.js
@@ -284,7 +284,6 @@ export const reptileMenu = async (v, language = "zh-CN") => {
  * @param {Array} menu 菜单
  */
 export const reptileCSSVariables = async (v, language = "zh-CN", menu) => {
-  const webUrl = v === "v4" ? VANT_WEBSITE_GITEE : VANT_WEBSITE;
   let menus = [];
   if (menu === undefined) {
     menus = await reptileMenu(v, language);
@@ -300,56 +299,45 @@ export const reptileCSSVariables = async (v, language = "zh-CN", menu) => {
     const children = menus[group].children;
     if (children && children.length > 0) {
       while (item < children.length) {
-        await page
-          .goto(`${webUrl}/${v}/#${children[item].value}`, {
-            waitUntil: "networkidle2",
-          })
-          .then(() => {
-            let el;
-            if (language === "zh-CN") {
-              el = "#yang-shi-bian-liang";
-            } else {
-              if (v === "v2") {
-                el = "#less-variables";
-              } else {
-                el = "#css-variables";
-              }
+        await page.goto(`${VANT_WEBSITE}/${v}/#${children[item].value}`, {
+          waitUntil: "networkidle2",
+        });
+        let el;
+        if (language === "zh-CN") {
+          el = "#yang-shi-bian-liang";
+        } else {
+          if (v === "v2") {
+            el = "#less-variables";
+          } else {
+            el = "#css-variables";
+          }
+        }
+        const style = await page.evaluate((el) => {
+          const ysbl = document.querySelector(el);
+          const stylesList = [];
+          if (ysbl) {
+            const styleGroup = ysbl.nextElementSibling.nextElementSibling
+              .querySelector("tbody")
+              .querySelectorAll("tr");
+            const itemEl = Array.from(styleGroup);
+            for (let j = 0; j < itemEl.length; j++) {
+              stylesList.push({
+                label: itemEl[j].querySelector("td").innerText,
+                value: "",
+              });
             }
-            return page.evaluate((el) => {
-              // const el =
-              //   language === "zh-CN"
-              //     ? document.querySelector("#yang-shi-bian-liang")
-              //     : document.querySelector("#css-variables");
-              const ysbl = document.querySelector(el);
-              if (!ysbl) {
-                return [];
-              } else {
-                const styleGroup = ysbl.nextElementSibling.nextElementSibling
-                  .querySelector("tbody")
-                  .querySelectorAll("tr");
-                const stylesList = [],
-                  itemEl = Array.from(styleGroup);
-                for (let j = 0; j < itemEl.length; j++) {
-                  stylesList.push({
-                    label: itemEl[j].querySelector("td").innerText,
-                    value: "",
-                  });
-                }
-                return stylesList;
-              }
-            }, el);
-          })
-          .then((style) => {
-            if (style.length > 0) {
-              const styleItem = {
-                label: children[item].label,
-                value: children[item].value,
-                children: style,
-              };
-              styles.push(styleItem);
-            }
-            item++;
-          });
+          }
+          return Promise.resolve(stylesList);
+        }, el);
+        if (style.length > 0) {
+          const styleItem = {
+            label: children[item].label,
+            value: children[item].value,
+            children: style,
+          };
+          styles.push(styleItem);
+        }
+        item++;
       }
     }
     group++;
