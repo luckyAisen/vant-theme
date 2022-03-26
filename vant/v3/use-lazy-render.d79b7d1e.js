9724a8451370dfb5165749a23fd6ac9400e5cf9a
Aisen60 <Aisen60@users.noreply.github.com>
2022-03-26 08:43:27 +0000

    deploy: 9a7633298dc0bd8d4c8259ee2b48d9d7c6cad7e4

---
+++ vant/v3/use-lazy-render.d79b7d1e.js
@@ -0,0 +1,1 @@
+import{D as r,x as i}from"./vue-libs.bf80eadf.js";function u(a){const t=r(!1);return i(a,e=>{e&&(t.value=e)},{immediate:!0}),e=>()=>t.value?e():null}export{u};
---
+++ vant/v4/deep-clone.5ed53646.js
@@ -0,0 +1,1 @@
+import{i as f,f as n}from"./use-translate.94d81043.js";function i(r){if(!f(r))return r;if(Array.isArray(r))return r.map(e=>i(e));if(n(r)){const e={};return Object.keys(r).forEach(t=>{e[t]=i(r[t])}),e}return r}export{i as d};
