da5a796f502196bf2ceb6b3f30d3fd7fc6e8d779
Aisen60 <caixusheng60@gmail.com>
2024-01-18 18:22:57 +0800

    feat: create workspace page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
       component: () => import('@/views/index.vue')
+    },
+    {
+      path: '/workspace/:version/:id',
+      name: 'workspace',
+      component: () => import('@/views/workspace/index.vue')
     }
   ]
 });
---
+++ src/views/workspace/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="workspance"></div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'Workspance'
+});
+</script>
+
+<style lang="scss" scoped>
+.workspance {
+  width: 100vw;
+  height: 100vh;
+  background-color: #efefef;
+  overflow: hidden;
+}
+</style>
