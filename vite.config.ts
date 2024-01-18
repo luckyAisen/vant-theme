aceacf11e70b4c8d6eebfe83b1f2d6e8973c4d89
Aisen60 <caixusheng60@gmail.com>
2024-01-18 18:07:04 +0800

    chore: modify vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import AutoImport from 'unplugin-auto-import/vite';
@@ -8,27 +8,34 @@ import Components from 'unplugin-vue-components/vite';
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    AutoImport({
-      imports: [
-        'vue',
-        {
-          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
-        }
-      ],
-      dts: './types/auto-imports.d.ts'
-    }),
-    Components({
-      resolvers: [NaiveUiResolver()],
-      dts: './types/components.d.ts'
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.VITE_BASE_URL,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        imports: [
+          'vue',
+          {
+            'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
+          }
+        ],
+        dts: './types/auto-imports.d.ts'
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()],
+        dts: './types/components.d.ts'
+      })
+    ],
+    server: {
+      host: '0.0.0.0'
     }
-  }
+  };
 });
