c1654853b92afad75f8706059844a0b2c1e05efd
Aisen60 <caixusheng60@gmail.com>
2024-02-05 14:59:30 +0800

    chore: modify some config

--- types/components.d.ts
+++ types/components.d.ts
@@ -7,13 +7,23 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    Header: typeof import('./../src/components/Header/index.vue')['default']
+    Locale: typeof import('./../src/components/locale.vue')['default']
+    LocaleSvg: typeof import('./../src/components/LocaleSvg/index.vue')['default']
+    LogoSvg: typeof import('./../src/components/LogoSvg/index.vue')['default']
     NButton: typeof import('naive-ui')['NButton']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
     NDatePicker: typeof import('naive-ui')['NDatePicker']
     NDialogProvider: typeof import('naive-ui')['NDialogProvider']
+    NForm: typeof import('naive-ui')['NForm']
+    NFormItem: typeof import('naive-ui')['NFormItem']
+    NIcon: typeof import('naive-ui')['NIcon']
+    NInput: typeof import('naive-ui')['NInput']
     NLoadingBarProvider: typeof import('naive-ui')['NLoadingBarProvider']
     NMessageProvider: typeof import('naive-ui')['NMessageProvider']
+    NModal: typeof import('naive-ui')['NModal']
     NNotificationProvider: typeof import('naive-ui')['NNotificationProvider']
+    NSelect: typeof import('naive-ui')['NSelect']
     NSpace: typeof import('naive-ui')['NSpace']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
